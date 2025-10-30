774f1c07e396fa064bb10222970d35ac33767c05
AnttiN <fanno004@edu.xamk.fi>
2025-10-30 09:10:07 +0200

    PlayerController and FPS Camera added

---
+++ Assets/Scsripts/Player.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float mouseSensitivity = 30f;
+    [SerializeField] private float gravityMultiplier = 30f;
+    [SerializeField] private float jumpForce = 50f;
+    [SerializeField] private float verticalLookRange = 80f;
+
+    private Vector3 moveInput;
+    private Vector2 lookDeltaInput;
+    private Vector3 jumpVelocity;
+    //private float verticalVelocity = 0f;
+    private float verticalRotation = 0f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        var rawInput = inputValue.Get<Vector2>();
+        moveInput = new(rawInput.x, 0f, rawInput.y);
+    }
+
+    public void OnLook(InputValue inputValue)
+    {
+        lookDeltaInput = inputValue.Get<Vector2>() * mouseSensitivity;
+    }
+
+    public void OnJump()
+    {
+        if (characterController.isGrounded)
+        {
+            jumpVelocity = (transform.forward + transform.up) * jumpForce;
+            //verticalVelocity += jumpForce;
+        }
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0f, lookDeltaInput.x * Time.deltaTime, 0f);
+        if (!Mathf.Approximately(lookDeltaInput.y, 0f))
+        {
+            verticalRotation = Mathf.Clamp(verticalRotation - lookDeltaInput.y * Time.deltaTime, -verticalLookRange, verticalLookRange);
+            Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        }
+        
+        var movement = (transform.forward * moveInput.z + transform.right * moveInput.x);
+        if (characterController.isGrounded && jumpVelocity.y < 0f)
+        {
+            jumpVelocity = Vector3.down;
+            //verticalVelocity = -1f;
+        }
+        else
+        {
+            //verticalVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+            jumpVelocity += Physics.gravity * gravityMultiplier * Time.deltaTime;
+        }
+
+        movement += jumpVelocity;
+        //movement.y = verticalVelocity;
+
+        characterController.Move(Time.deltaTime * movementSpeed * movement);
+    }
+}
